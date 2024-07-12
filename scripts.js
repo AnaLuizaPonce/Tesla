2a06def46cd77818a481cc9cbfa7e8782a381387
AnaLuizaPonce <a.luiza.ponce@gmail.com>
2024-07-11 23:54:00 -0300

    Ajuste Form

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 const formulario = document.querySelector(".formulario");
 
 function aparecerformulario() {
-	formulario.style.left = "65%";
+	formulario.style.left = "58%";
 }
 
 function desaparecerformulario() {
-	formulario.style.left = "-65%";
+	formulario.style.left = "-58%";
 }
