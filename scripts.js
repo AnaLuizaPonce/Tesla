91d0d7988573d7355f8575493e0f7df48dcff664
AnaLuizaPonce <a.luiza.ponce@gmail.com>
2024-07-11 23:49:43 -0300

    Add Form

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const formulario = document.querySelector(".formulario");
+
+function aparecerformulario() {
+	formulario.style.left = "65%";
+}
+
+function desaparecerformulario() {
+	formulario.style.left = "-65%";
+}
